f91ef9dad0f3d159369021d5d45d1f11d7223b98
Khushi Donda <khushidonda02@gmail.com>
2026-06-11 17:14:43 +0000

    Initial project setup and Load Dataset and Explore Dataset

--- Car_Price_Prediction_with_Machine_Learning.py
+++ Car_Price_Prediction_with_Machine_Learning.py
@@ -1,3 +1,4 @@
+#!/workspaces/Car_Price_Prediction_with_Machine_Learning/.venv/bin/python
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
@@ -7,3 +8,14 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
+
+# Load Dataset
+df = pd.read_csv("car data.csv")
+
+print(df.head())
+
+# Explore Dataset
+print(df.info())
+print(df.describe())
+print(df.isnull().sum())
+
---
+++ run.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+.venv/bin/python Car_Price_Prediction_with_Machine_Learning.py
