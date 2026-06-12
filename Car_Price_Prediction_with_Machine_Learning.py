f3d56d48aa143e5382adeca83a78620924a68a12
Khushi Donda <khushidonda02@gmail.com>
2026-06-12 09:29:26 +0000

    Add all features

--- Car_Price_Prediction_with_Machine_Learning.py
+++ Car_Price_Prediction_with_Machine_Learning.py
@@ -33,3 +33,44 @@ df['Car_Age'] = df['Current_Year'] - df['Year']
 
 df.drop(['Year', 'Car_Name', 'Current_Year'], axis=1, inplace=True)
 
+# Visualization
+# Correlation Heatmap       
+plt.figure(figsize=(10,8))
+sns.heatmap(df.corr(), annot=True, cmap='coolwarm')
+plt.title("Correlation Heatmap")
+plt.show() 
+
+ # Define Features and Target                  
+X = df.drop('Selling_Price', axis=1)
+y = df['Selling_Price'] 
+
+# Split Dataset                    
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42
+)                         
+
+# Train Regression Model                        
+model = LinearRegression()
+model.fit(X_train, y_train)  
+
+# Make Predictions                                         
+y_pred = model.predict(X_test)
+
+print(y_pred[:5])
+
+# Model Evaluation
+mae = mean_absolute_error(y_test, y_pred)
+mse = mean_squared_error(y_test, y_pred)
+r2 = r2_score(y_test, y_pred)
+
+print("MAE :", mae)
+print("MSE :", mse)
+print("R2 Score :", r2)
+
+# Actual vs Predicted Graph
+plt.figure(figsize=(6,6))
+plt.scatter(y_test, y_pred)
+plt.xlabel("Actual Price")
+plt.ylabel("Predicted Price")
+plt.title("Actual vs Predicted Car Prices")
+plt.show()
